6bba293b5d430ebd8dc428d1bdc93e67cf5333ef
Aman <tripathiaman555@gmail.com>
2023-07-01 23:58:39 +0530

    Added About Link

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter} from 'react-router-dom'
+import {BrowserRouter as Router, Route,Routes} from 'react-router-dom'
 import {v4 as uuidv4} from 'uuid'
 import { useState } from "react"
 import Header from "./Components/Header"
@@ -7,6 +7,7 @@ import FeedbackStats from "./Components/FeedbackStats"
 import FeedbackData from "./DATA/FeedbackData";
 import FeedbackForm from "./Components/FeedbackForm";
 import AboutPage from './Pages/AboutPage'
+import AboutIconLink from './Components/AboutIconLink'
 
 function App(){
     const [feedback, setFeedback] = useState(FeedbackData);
@@ -24,16 +25,23 @@ function App(){
 
 
     return (
-    <>
+    <Router>
         <Header/>
         <div className="container">
-            <FeedbackForm handleAdd={addFeedback}/>
-            <FeedbackStats feedback = {feedback}/>
-            <FeedbackList feedback = {feedback}
-                handleDelete={deleteFeedback}/>
-            <AboutPage/>̥
+            <Routes>
+                <Route exact path='/' element = {
+                <>
+                    <FeedbackForm handleAdd={addFeedback}/>
+                    <FeedbackStats feedback = {feedback}/>
+                    <FeedbackList feedback = {feedback} handleDelete={deleteFeedback}/>
+                </>
+                } >
+                </Route>
+                ̥<Route path = '/about' element={<AboutPage/>}/>
+            </Routes>
+            <AboutIconLink/>
         </div>
-    </>
+    </Router>
     )
 }
 export default App
